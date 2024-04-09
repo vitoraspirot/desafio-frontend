2a4c6e2d616ecfd296c3a12405e6c1819370b66a
Wesley Gabriel Boeira da Rocha <47437871+WesleyGBR@users.noreply.github.com>
2024-04-09 03:24:53 -0300

    Update index.jsx from Student's home
    Followed the scoped developed in Figma.

--- src/pages/student/home/index.jsx
+++ src/pages/student/home/index.jsx
@@ -1,16 +1,80 @@
-import { Typography } from "@mui/material";
+import { Button, Typography, Box, AppBar, Container, Toolbar, Stack } from "@mui/material";
 import { Header } from "../../../components/header";
 import { Main } from "../../../components/main";
 
-export function StudentHome() {
+export function StudentsHome() {
   return (
     <>
       <Header items={[{ label: "INÍCIO", route: "/" }]} />
 
       <Main>
-        <Typography variant="h3" color="black">
-          Página do estudante
-        </Typography>
+        <AppBar
+          position="static"
+          color="primary"
+          style={{ backgroundColor: 'white', color: '#014A7F' }}
+        >
+          <Toolbar>
+            <Typography variant="h6" style={{ flexGrow: 1 }}>
+              Uniesquina
+            </Typography>
+            <Button color="inherit">Sair</Button>
+          </Toolbar>
+        </AppBar>
+        <Container
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            minHeight: '100vh',
+          }}
+        >
+          <Stack direction="row" spacing={2}>
+            <Button
+              variant="contained"
+              style={{
+                backgroundColor: 'white',
+                color: '#014A7F',
+                width: 308,
+                height: 334,
+              }}
+            >
+              <Box height="100%" display="flex" alignItems="flex-end">
+                Minhas turmas
+              </Box>
+            </Button>
+            <Button
+              variant="contained"
+              style={{
+                backgroundColor: 'white',
+                color: '#014A7F',
+                width: 308,
+                height: 334,
+              }}
+            >
+              <Box height="100%" display="flex" alignItems="flex-end">
+                Matricular-se
+              </Box>
+            </Button>
+          </Stack>
+        </Container>
+        <AppBar
+          position="static"
+          color="primary"
+          style={{ top: 'auto', bottom: 0 }}
+        >
+          <Toolbar
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              backgroundColor: '#014A7F',
+            }}
+          >
+            <Typography variant="body1" color="inherit">
+              Designed and developed by Vitor Aspirot, Angelo and Wesley Rocha
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </Main>
     </>
   );
