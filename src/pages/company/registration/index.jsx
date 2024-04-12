f5a879c7355d57f1e5f58315b92094b413abf9f6
Vitor Aspirot <vitor.trindade@cwi.com.br>
2024-04-12 20:08:05 -0300

    ✨ feature: Implementa form no Cadastro de turma

--- src/pages/company/registration/index.jsx
+++ src/pages/company/registration/index.jsx
@@ -1,17 +1,50 @@
-import {
-  Typography,
-  FormControl,
-  Select,
-  InputLabel,
-  MenuItem,
-  Stack,
-  Button,
-} from "@mui/material";
+import { Typography, MenuItem, Stack, Button, TextField } from "@mui/material";
 import { Header } from "../../../components/Header";
 import { Main } from "../../../components/Main";
-import { UniTextField } from "../../../components/UniTextField";
+import { useState } from "react";
 
 export function RegisterClass() {
+  const [discipline, setDiscipline] = useState("");
+  const [teacher, setTeacher] = useState("");
+  const [semester, setSemester] = useState("");
+  const [shift, setShift] = useState("");
+  const [day, setDay] = useState("");
+  const [disciplineError, setDisciplineError] = useState(false);
+  const [teacherError, setTeacherError] = useState(false);
+  const [semesterError, setSemesterError] = useState(false);
+  const [shiftError, setShiftError] = useState(false);
+  const [dayError, setDayError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setDisciplineError(false);
+    setTeacherError(false);
+    setSemesterError(false);
+    setShiftError(false);
+    setDayError(false);
+
+    if (discipline === "") {
+      setDisciplineError(true);
+    }
+    if (teacher === "") {
+      setTeacherError(true);
+    }
+    if (semester === "") {
+      setSemesterError(true);
+    }
+    if (shift === "") {
+      setShiftError(true);
+    }
+    if (day === "") {
+      setDayError(true);
+    }
+
+    if (discipline && teacher && semester && shift && day) {
+      console.log("Cadastrado!");
+    }
+  };
+
   return (
     <>
       <Header items={[{ label: "INÍCIO", route: "/" }]} />
@@ -27,53 +60,83 @@ export function RegisterClass() {
             Cadastrar turma
           </Typography>
         </Stack>
-        <Stack padding="0 200px" alignItems="center" justifyContent="center">
-          <UniTextField label="Disciplina" />
 
-          <UniTextField label="Professor(a)" />
-
-          <UniTextField label="Semestre" />
-
-          <FormControl
-            variant="outlined"
-            sx={{ width: "400px", margin: "16px 0" }}
-          >
-            <Select
-              label="Turno"
-              defaultValue=""
-              style={{ width: "394px", height: "60px" }}
+        <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+          <Stack padding="0 200px" alignItems="center" justifyContent="center">
+            <TextField
+              label="Disciplina"
+              variant="outlined"
               required
+              fullWidth
+              margin="normal"
+              sx={{ width: "400px" }}
+              onChange={(e) => {
+                setDiscipline(e.target.value);
+              }}
+              error={disciplineError}
+            />
+
+            <TextField
+              label="Professor(a)"
+              variant="outlined"
+              required
+              fullWidth
+              margin="normal"
+              sx={{ width: "400px" }}
+              onChange={(e) => setTeacher(e.target.value)}
+              error={teacherError}
+            />
+
+            <TextField
+              label="Semestre"
+              variant="outlined"
+              required
+              fullWidth
+              margin="normal"
+              sx={{ width: "400px" }}
+              onChange={(e) => setSemester(e.target.value)}
+              error={semesterError}
+            />
+
+            <TextField
+              label="Turno"
+              required
+              select
+              margin="normal"
+              sx={{ width: "400px" }}
+              onChange={(e) => setShift(e.target.value)}
+              error={shiftError}
             >
               <MenuItem value="Manhã">Manhã</MenuItem>
+              <MenuItem value="Tarde">Tarde</MenuItem>
               <MenuItem value="Noite">Noite</MenuItem>
-            </Select>
-            <InputLabel id="turno-label">Turno</InputLabel>
-          </FormControl>
+            </TextField>
 
-          <FormControl
-            variant="outlined"
-            sx={{ width: "400px", marginBottom: "32px" }}
-          >
-            <InputLabel id="diaSemana-label">Dia da semana</InputLabel>
-            <Select
-              labelId="diaSemana-label"
-              id="diaSemana"
-              name="diaSemana"
+            <TextField
               label="Dia da semana"
-              defaultValue=""
-              style={{ width: "394px", height: "60px" }}
+              margin="normal"
+              sx={{ width: "400px", marginBottom: "16px" }}
               required
+              select
+              onChange={(e) => setDay(e.target.value)}
+              error={dayError}
             >
               <MenuItem value="Segunda-feira">Segunda-feira</MenuItem>
               <MenuItem value="Terça-feira">Terça-feira</MenuItem>
               <MenuItem value="Quarta-feira">Quarta-feira</MenuItem>
               <MenuItem value="Quinta-feira">Quinta-feira</MenuItem>
               <MenuItem value="Sexta-feira">Sexta-feira</MenuItem>
-            </Select>
-          </FormControl>
+            </TextField>
 
-          <Button variant="contained">Cadastrar</Button>
-        </Stack>
+            <Button
+              variant="contained"
+              type="submit"
+              onClick={() => console.log("Click!")}
+            >
+              Cadastrar
+            </Button>
+          </Stack>
+        </form>
       </Main>
     </>
   );
